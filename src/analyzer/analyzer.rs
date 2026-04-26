3c7a921b8d811d856b0df678b7a3269cca62c3fb
Anatoly Sergeev (miruji) <miruji@protonmail.com>
2026-04-27 02:53:40 +0300

    Add analyzer 3

--- src/analyzer/analyzer.rs
+++ src/analyzer/analyzer.rs
@@ -1,11 +1,28 @@
+use std::collections::HashSet;
 use std::sync::{RwLock, RwLockReadGuard};
 use std::sync::Arc;
+use lazy_static::lazy_static;
 use wasm_bindgen::prelude::wasm_bindgen;
 use serde::Serialize;
 use serde_json::to_string;
 use crate::tokenizer::line::Line;
-use crate::tokenizer::token::Token;
+use crate::tokenizer::token::{Token, TokenType};
 use crate::tokenizer::tokenizer::readTokens;
+// =================================================================================================
+
+lazy_static!
+{
+  /// Множество встроенных процедур и функций
+  ///
+  /// todo Должно автоматически собираться из парсера
+  static ref Builtins: HashSet<&'static str> = {
+    HashSet::from([
+      "println", "print", "clear", "go", "sleep", "exit",
+      "type", "mut", "randUInt", "len", "input", "exec", "execs"
+    ])
+  };
+}
+
 // =================================================================================================
 
 /// Выходная сериализуемая структура
@@ -22,7 +39,13 @@ pub struct AnalyzeToken
 #[wasm_bindgen]
 pub fn tokenize(code: &str) -> String
 {
-  let buffer: Vec<u8> = code.as_bytes().to_vec();
+  let mut buffer: Vec<u8> = code.as_bytes().to_vec();
+  // Добавляем перевод строки, если его нет в конце
+  if buffer.last() != Some(&b'\n') {
+    buffer.push(b'\n');
+  }
+  
+  //
   let lines: Vec< Arc<RwLock<Line>> > = readTokens(buffer, false);
   let mut result: Vec<AnalyzeToken> = Vec::new();
   flattenLines(&lines, &mut result);
@@ -49,17 +72,31 @@ fn flattenLines(lines: &[Arc<RwLock<Line>>], out: &mut Vec<AnalyzeToken>)
   //
 }
 
-/// Обход списка токенов, включая их внутренние линии (f‑строки)
+/// Обход списка токенов, включая их внутренние линии
 fn flattenTokens(tokens: &[Token], out: &mut Vec<AnalyzeToken>) 
 {
   for token in tokens 
   {
+    // Определяем kind, возможно заменяя Word на BuiltinProcedure
+    let mut kind = token.getDataType().to_string();
+    if token.getDataType() == &TokenType::Word 
+    {
+      if let Some(data) = token.getData().toString() 
+      {
+        if Builtins.contains(data.as_str()) 
+        {
+          kind = String::from("Builtin");
+        }
+        //
+      }
+    }
+    
     // Добавляем сам токен
     #[cfg(feature = "analyzer")]
     out.push(AnalyzeToken {
       start: token.start,
       end: token.end,
-      kind: token.getDataType().to_string(),
+      kind
     });
 
     // Если у токена есть вложенные линии
